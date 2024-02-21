d9c022b5120eb07ecf42d3b80f9910de017faa6f
Maxim <maximgratii@gmail.com>
2024-02-21 22:53:25 +0200

    fix

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 export const Feedefy = {
   props: {
-    projectId: String
+    projectId: String,
+    required: true,
   },
 
   mounted() {
-    if (!this.props.projectId) {
+    if (this.$props) { // For Vue 2
+      projectId = this.$props.projectId;
+    } else if (this.props) { // For Vue 3
+      projectId = this.props.projectId;
+    }
+
+    if (!projectId) {
       console.error("No projectId was passed to Feedefy Vue, widget will not be initialized");
       return;
     }
 
     const script = document.createElement("script");
 
-    script.setAttribute("src", `https://app.fedeefy.com/widget.js?id=${this.props.projectId}`);
+    script.setAttribute("src", `https://app.fedeefy.com/widget.js?id=${projectId}`);
 
     script.defer = true;
 
