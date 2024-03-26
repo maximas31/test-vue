546534c9439fd4d498c08b07e2bc68ee2c44176e
Maxim <maximgratii@gmail.com>
2024-03-26 18:29:20 +0200

    add lang prop

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export const Feedefy = {
     projectId: {
       type: String,
       required: true
+    },
+    lang: {
+      type: String,
+      required: false
     }
   },
 
@@ -18,13 +22,16 @@ export const Feedefy = {
     }
 
     let id = "";
+    let lang = "";
 
     if (this.$props) {
       // For Vue 2
       id = this.$props.projectId;
+      lang = this.$props.lang;
     } else if (this.props) {
       // For Vue 3
       id = this.props.projectId;
+      lang = this.props.lang;
     }
 
     if (!id) {
@@ -33,6 +40,11 @@ export const Feedefy = {
     }
 
     this.script = document.createElement("script");
+
+    if (lang) {
+      this.script.setAttribute("lang", lang);
+    }
+
     this.script.setAttribute("src", `https://app.feedefy.com/embed.js?id=${id}`);
     this.script.defer = true;
     this.script.addEventListener("error", () => this.script.remove());
